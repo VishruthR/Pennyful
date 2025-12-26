3715ca835f33c95401754a982c74ecb85a397789
Vishruth Raj <vishraj248@gmail.com>
2025-12-26 09:08:15 -0800

    Finish amex importer

--- src-tauri/src/importers/american_express.rs
+++ src-tauri/src/importers/american_express.rs
@@ -13,8 +13,6 @@ pub fn parse_csv_statement<P: AsRef<Path>>(filename: P) -> Result<Vec<Transactio
     let transactions: Vec<TransactionImport> = reader
         .records()
         .filter_map(|item| item.ok())
-        // Skip header line
-        .skip(1)
         .filter_map(|transaction_record| transaction_record.deserialize(Some(&headers)).ok())
         .map(|transaction: TransactionImport| TransactionImport { 
             date: transaction.date, 
@@ -40,99 +38,65 @@ mod tests {
         let transactions_path = PathBuf::from(TRANSACTIONS_PATH);
         let transactions_expected = vec![
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 10).unwrap(),
-                name: "Zelle payment from JOHN DOE for food\"; Conf# 11bu1u1\"".to_string(),
-                amount: dec!(5),
+                date: NaiveDate::from_ymd_opt(2025, 12, 23).unwrap(),
+                name: "UBERBV              CITY           ST".to_string(),
+                amount: dec!(-6.71),
             },
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 10).unwrap(),
-                name: "VENMO DES:PAYMENT ID:XXXXX INDN:JOHN DOE CO ID:XXXXX WEB".to_string(),
-                amount: dec!(-10),
+                date: NaiveDate::from_ymd_opt(2025, 12, 22).unwrap(),
+                name: "XXXX#FLIGHT.AIRLINE               FA".to_string(),
+                amount: dec!(-2404.89),
             },
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 10).unwrap(),
-                name: "VENMO DES:PAYMENT ID:XXXXX INDN:JOHN DOE CO ID:XXXXX WEB".to_string(),
-                amount: dec!(-7.20),
+                date: NaiveDate::from_ymd_opt(2025, 12, 22).unwrap(),
+                name: "AMEX Airline Fee Reimbursement".to_string(),
+                amount: dec!(27.60),
             },
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 10).unwrap(),
-                name: "VENMO DES:PAYMENT ID:XXXXX INDN:JOHN DOE CO ID:XXXXX WEB".to_string(),
-                amount: dec!(-0.60),
+                date: NaiveDate::from_ymd_opt(2025, 12, 22).unwrap(),
+                name: "AplPay UMM PLAZA GIFCITY             ST".to_string(),
+                amount: dec!(-6.62),
             },
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 12).unwrap(),
-                name: "COSTCO WHSE#1111 11/11 PURCHASE CITY ST".to_string(),
-                amount: dec!(-141.02),
+                date: NaiveDate::from_ymd_opt(2025, 12, 21).unwrap(),
+                name: "SOME AIRLINES   XXX-XXX-XXXX        ST".to_string(),
+                amount: dec!(-198.48),
             },
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 12).unwrap(),
-                name: "WELLS FARGO CARD DES:CRD ID:XXXXXXXXXX INDN:JOHN DOE CO ID:XXXXX WEB".to_string(),
-                amount: dec!(-481.86),
+                date: NaiveDate::from_ymd_opt(2025, 12, 21).unwrap(),
+                name: "AMEX CLEAR PLUS CREDIT".to_string(),
+                amount: dec!(209.00),
             },
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 17).unwrap(),
-                name: "VENMO DES:CASHOUT ID:XXXXX INDN:JOHN DOE CO ID:XXXXX PPD".to_string(),
-                amount: dec!(906.30),
+                date: NaiveDate::from_ymd_opt(2025, 12, 21).unwrap(),
+                name: "CLEAR *CLEARME.COM  CITY            ST".to_string(),
+                amount: dec!(-209.00),
             },
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 17).unwrap(),
-                name: "COSTCO WHSE#1111 11/14 PURCHASE CITY ST".to_string(),
-                amount: dec!(-4.44),
+                date: NaiveDate::from_ymd_opt(2025, 12, 21).unwrap(),
+                name: "HALF PRICE THINGS    CITY            ST".to_string(),
+                amount: dec!(-4.41),
             },
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 21).unwrap(),
-                name: "SimpleBills Prod DES:WEB PMTS ID:AAAA INDN:JOHN DOE CO ID:XXXXX WEB".to_string(),
-                amount: dec!(-31.94),
+                date: NaiveDate::from_ymd_opt(2025, 12, 21).unwrap(),
+                name: "REGISTRATION FEE       CITY".to_string(),
+                amount: dec!(-355.00),
             },
             TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 25).unwrap(),
-                name: "WELLS FARGO CARD DES:CRD ID:XXXXXXXXXX INDN:JOHN DOE CO ID:XXXXX WEB".to_string(),
-                amount: dec!(-474.74),
-            },
-            TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 11, 28).unwrap(),
-                name: "VENMO DES:PAYMENT ID:XXXXX INDN:JOHN DOE CO ID:XXXXX WEB".to_string(),
-                amount: dec!(-49.50),
-            },
-            TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 12, 1).unwrap(),
-                name: "Online Recurring transfer from CHK 1111 Confirmation# aaaaaaa; DOE, JANE".to_string(),
-                amount: dec!(1000),
-            },
-            TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 12, 1).unwrap(),
-                name: "PAYPAL *NEWBALANCEA 11/27 PURCHASE 111-111-11111 MO".to_string(),
-                amount: dec!(-93.51),
-            },
-            TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 12, 4).unwrap(),
-                name: "BILTPYMTS DES:RENT PMT ID:XXXXXXXXXX INDN:JOHN DOE CO ID:XXXXX WEB".to_string(),
-                amount: dec!(-939.00),
-            },
-            TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 12, 8).unwrap(),
-                name: "PAYPAL DES:INST XFER ID:AMAZON INDN:JOHN DOE CO ID:PAYPALS WEB".to_string(),
-                amount: dec!(-53.84),
-            },
-            TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 12, 8).unwrap(),
-                name: "PAYPAL DES:INST XFER ID:EBAY INDN:JOHN DOE CO ID:PAYPALS WEB".to_string(),
-                amount: dec!(-46.13),
-            },
-            TransactionImport {
-                date: NaiveDate::from_ymd_opt(2025, 12, 9).unwrap(),
-                name: "WELLS FARGO CARD DES:CRD ID:XXXXXXXXXX INDN:JOHN DOE CO ID:XXXXX WEB".to_string(),
-                amount: dec!(-397.57),
+                date: NaiveDate::from_ymd_opt(2025, 12, 20).unwrap(),
+                name: "AplPay AIRLINE AIRLINCITY            ST".to_string(),
+                amount: dec!(-35.00),
             },
         ];
 
         let transactions = parse_csv_statement(&transactions_path)?;
 
-        for transaction in transactions {
-            println!("{}", transaction);
-        }
-
         // assert_eq!(transactions, transactions_expected);
+
+        use std::iter::zip;
+        for (transaction, expected) in zip(transactions, transactions_expected) {
+            assert_eq!(transaction, expected);
+        }
         Ok(())
     }
 }
