781a93635780253dcbd97bfb6cd9911e03d318eb
Vishruth Raj <vishraj248@gmail.com>
2026-09-19 21:47:39 -0400

    Undo changes to TopCategories + format parse utils

--- src/lib/utils/parse.ts
+++ src/lib/utils/parse.ts
@@ -1,9 +1,8 @@
-
 /** Parses a numeric string into a number.
-  * 
-  * Supports strings of the form 4,000.00
-  * TODO: Support different locales (e.g. ones that use commas to mark the decimal)
-  */
+ *
+ * Supports strings of the form 4,000.00
+ * TODO: Support different locales (e.g. ones that use commas to mark the decimal)
+ */
 export const parseNumericString = (numericString: string): number => {
   return parseFloat(numericString.replaceAll(",", ""));
-}
+};
